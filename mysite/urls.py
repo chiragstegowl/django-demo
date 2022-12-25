862992a7f72c0323c9d5bccf6c000386bd3979ad
chirag narang <chirag.stegow@gmail.com>
2022-12-25 12:33:42 +0530

    add health check

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def demo_function(request):
+    return HttpResponse(status = 200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', demo_function),
 ]
